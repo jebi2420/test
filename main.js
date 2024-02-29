568db4cb1bafab505345e3384be4ca80d5eb5148
jebi2420 <jebi2420@gmail.com>
2024-02-29 21:14:30 +0900

    [add] api 변경

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // 가장 뜨거운 코인 top3
-const myKey = "";
+//const myKey = "f3ca5cbf-842e-439f-829e-45f6a648fca2";
+let latestList = [];
 
 const getHotTop = async () => {
   const hotUrl = new URL(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`);
@@ -12,6 +13,7 @@ const getHotTop = async () => {
   console.log("response",response)
   const data = await response.json();
   console.log("data",data)
+  latestList = data.
 }
 
 getHotTop();
