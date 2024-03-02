d62d877b49a5eddf4e0f3e0e8503639e3c3d787e
jebi2420 <jebi2420@gmail.com>
2024-03-02 14:13:56 +0900

    [add] sort 전

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // 가장 뜨거운 코인 top3
-//const myKey = "f3ca5cbf-842e-439f-829e-45f6a648fca2";
+//const myKey = "12379dbf-30e0-4b89-92d0-b369a074adc1";
 let coinList = [];
 let response;
 
@@ -19,52 +19,101 @@ const getTopCoin = async () => {
     coinList = coinData.data;
     //console.log(coinList);
   
-    render()
+    topCoinRender()
   }catch(error){
     console.log("error message: " + error)
     errorRender(response.status);
   }
 }
 
-const render = () => {
-  // hot 3
-  const hotList = coinList.slice(0,3);
-  let hotHTML = ""
+// const render = () => {
+//   // hot 3
+//   const hotList = coinList.slice(0,3);
+//   let hotHTML = ""
 
-  for(i=0; i<hotList.length; i++){
-    hotHTML += `
-    <tr class="hot-list list">
-      <td>${hotList.indexOf(hotList[i])+1}</td>
-      <td id = "name">${hotList[i]["name"]}</td>
-      <td id = "symbol">${hotList[i]["symbol"]}</td>
-      <td id = "24h">${
+//   for(i=0; i<hotList.length; i++){
+//     hotHTML += `
+//     <tr class="hot-list list">
+//       <td>${hotList.indexOf(hotList[i])+1}</td>
+//       <td id = "name">${hotList[i]["name"]}</td>
+//       <td id = "symbol">${hotList[i]["symbol"]}</td>
+//       <td id = "24h">${
+//         hotList[i]["quote"].USD["percent_change_24h"].toFixed(2) + "%"
+//       }</td>
+//     </tr>
+//     `
+//   }
+
+//    document.getElementById("hot-container").innerHTML = hotHTML;
+
+//   // cold 3
+//   const coldList = coinList.slice(-3);
+//   let coldHTML = ""
+
+//   for(i=0; i<coldList.length; i++){
+//     coldHTML += `
+//     <tr class="cold-list list">
+//       <td>${coldList.indexOf(coldList[i])+1}</td>
+//       <td id = "name">${coldList[i]["name"]}</td>
+//       <td id = "symbol">${coldList[i]["symbol"]}</td>
+//       <td id = "24h">${
+//         coldList[i]["quote"].USD["percent_change_24h"].toFixed(2) + "%"
+//       }</td>
+//     </tr>
+//     `
+// document.getElementById("cold-container").innerHTML = coldHTML;
+
+// }
+// }
+
+
+// 가장 뜨거운 코인, 가장 차가운 코인 top3
+const topCoinRender = () => {
+  // hot top3
+  const hotList = coinList.slice(0, 3);
+  console.log("hotList: " + hotList);
+  let hotHTML = "";
+
+  for (i = 0; i < hotList.length; i++) {
+    hotHTML += `<div class="hot-list list">
+      <div class="coin-left">
+        <div class="coin-rank">${hotList.indexOf(hotList[i]) + 1}</div>
+        <div class="coin-names">
+          <div class="coin-name">${hotList[i]["name"]}</div>
+          <div class="coin-symbol">${hotList[i]["symbol"]}</div>
+        </div>
+      </div>
+      <div class="coin-24h">${
         hotList[i]["quote"].USD["percent_change_24h"].toFixed(2) + "%"
-      }</td>
-    </tr>
-    `
+      }</div>
+    </div>`;
   }
+  document.getElementById("hot-container").innerHTML = hotHTML;
 
-   document.getElementById("hot-container").innerHTML = hotHTML;
-
-  // cold 3
+  // cold top3
   const coldList = coinList.slice(-3);
-  let coldHTML = ""
+  console.log("coldList: " + coldList);
+  let coldHTML = "";
 
-  for(i=0; i<coldList.length; i++){
-    coldHTML += `
-    <tr class="cold-list list">
-      <td>${coldList.indexOf(coldList[i])+1}</td>
-      <td id = "name">${coldList[i]["name"]}</td>
-      <td id = "symbol">${coldList[i]["symbol"]}</td>
-      <td id = "24h">${
+  for (i = 0; i < coldList.length; i++) {
+    coldHTML += `<div class="cold-list list">
+      <div class="coin-left">
+        <div class="coin-rank">${coldList.indexOf(coldList[i]) + 1}</div>
+        <div class="coin-names">
+          <div class="coin-name">${coldList[i]["name"]}</div>
+          <div class="coin-symbol">${coldList[i]["symbol"]}</div>
+        </div>
+      </div>
+      <div class="coin-24h">${
         coldList[i]["quote"].USD["percent_change_24h"].toFixed(2) + "%"
-      }</td>
-    </tr>
-    `
-document.getElementById("cold-container").innerHTML = coldHTML;
+      }</div>
+    </div>`;
+  }
+  document.getElementById("cold-container").innerHTML = coldHTML;
+};
 
-}
-}
+
+
 
 const errorRender = (errorMessage) => {
   const errorHTML = `<div>
